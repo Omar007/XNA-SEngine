e35aad288a791d76dc35a77d15d5e4ab52f5b0b4
Omar Pakker <omarp@users.sourceforge.net>
2012-06-21 14:03:50 +0200

    SEngine Content added. Started on some classes.

---
+++ SEngine/Base/BaseObject.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Xna.Framework.Content;
+
+namespace SEngine.Base
+{
+	public abstract class BaseObject
+	{
+		#region Fields
+		//public const long flag1 = 0x00000001;
+
+		private Guid guid;
+
+		private long flags;
+		#endregion
+
+		#region Properties
+		[ContentSerializerIgnore]
+		[Browsable(false)]
+		public Guid Guid { get { return guid; } }
+
+		[ContentSerializerIgnore]
+		[Browsable(false)]
+		public string GuidString
+		{
+			get { return guid.ToString(); }
+			set { guid = new Guid(value); }
+		}
+
+		[ContentSerializerIgnore]
+		[Browsable(false)]
+		public long Flags
+		{
+			get { return flags; }
+			set { flags = value; }
+		}
+		#endregion
+
+		public BaseObject()
+		{
+			guid = Guid.NewGuid();
+			flags = 0;
+		}
+
+		public void SetFlag(long flag, bool enabled)
+		{
+			if (enabled)
+				flags |= flag;
+			else
+				flags &= ~flag;
+		}
+
+		public bool HasFlag(long flag)
+		{
+			return (flags & flag) != 0;
+		}
+
+		public void GenerateGuid()
+		{
+			guid = Guid.NewGuid();
+		}
+	}
+}
---
+++ SEngine/SEngine.cs
@@ -0,0 +1,56 @@
+﻿using SEngine.Base;
+using SEngine.Screens;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace SEngine
+{
+	public class SEngine
+	{
+		private static SEngine sEngine;
+		internal static SEngine SEngine { get { return sEngine; } }
+
+		public static SEngine CreateInstance(Game game, ContentManager contentManager, ContentManager levelContentManager)
+		{
+			if (sEngine == null)
+			{
+				sEngine = new SEngine(game, contentManager, levelContentManager);
+			}
+			else
+			{
+				sEngine.gameData = new GameData(graphicsDevice, contentManager, levelContentManager);
+			}
+			return sEngine;
+		}
+
+		#region Fields
+		private GameData gameData;
+
+		private SimpleConsole console;
+		#endregion
+
+		#region Properties
+		public GameData GameData { get { return gameData; } }
+
+		public SimpleConsole Console { get { return console; } }
+		#endregion
+
+		private SEngine(Game game, ContentManager contentManager, ContentManager levelContentManager)
+		{
+			gameData = new GameData(game.GraphicsDevice, contentManager, levelContentManager);
+
+			console = new SimpleConsole(game);
+		}
+
+		public void update(GameTime gameTime)
+		{
+			console.Draw(gameTime);
+		}
+
+		public void draw(GameTime gameTime)
+		{
+			console.Update(gameTime);
+		}
+	}
+}
