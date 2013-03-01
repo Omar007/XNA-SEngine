ea7e38b8cf6369b0bdae4aec17eaefeaf3bf975e
Omar Pakker <omarp@users.sourceforge.net>
2013-03-01 18:45:06 +0100

    Pending commit

---
+++ SEngine/Graphics/Quad.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SEngine.Graphics
+{
+	internal class Quad
+	{
+		/*private static readonly VertexPositionTexture[] vertices = {
+			new VertexPositionTexture(new Vector3(-1, -1, 0), Vector2.UnitY),
+			new VertexPositionTexture(new Vector3(1, -1, 0), Vector2.One),
+			new VertexPositionTexture(new Vector3(-1, 1, 0), Vector2.Zero),
+			new VertexPositionTexture(new Vector3(1, 1, 0), Vector2.UnitX)
+		};*/
+
+		private static readonly VertexPositionTexture[] vertices = {
+			new VertexPositionTexture(new Vector3(-1, 1, 1), Vector2.Zero),
+			new VertexPositionTexture(new Vector3(1, 1, 1), Vector2.UnitX),
+			new VertexPositionTexture(new Vector3(-1, -1, 1), Vector2.UnitY),
+			new VertexPositionTexture(new Vector3(1, -1, 1), Vector2.One)
+		};
+
+		public static void RenderFullscreenQuad(GraphicsDevice graphicsDevice)
+		{
+			graphicsDevice.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleStrip, vertices, 0, 2);
+		}
+	}
+}
---
+++ SEngine/Graphics/Renderer/DefferedReentrantRenderer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace SEngine.Graphics.Renderer
+{
+	internal class DefferedReentrantRenderer
+	{
+		private RenderTarget2D gBuffer;
+
+		internal DefferedReentrantRenderer(GraphicsDevice graphicsDevice)
+		{
+			gBuffer = new RenderTarget2D(graphicsDevice, 1920, 1080, false, SurfaceFormat.HdrBlendable, DepthFormat.Depth24Stencil8, 0, RenderTargetUsage.DiscardContents);
+		}
+	}
+}
